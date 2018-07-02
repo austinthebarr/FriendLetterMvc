00163d240a7bd7b5c049a09dc44a4077fa045133
austinbarr@protonmail.com <austinbarr@Nichelles-MacBook-Pro.local>
2018-07-02 10:56:03 -0700

    followed along with lesson

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+    // [Route("/hello")]
+    // public string Hello() { return "Hello friend!"; }
+
+    // [Route("/goodbye")]
+    // public string Goodbye() {return "Goodbye friend."; }
+
+    // [Produces("text/html")]
+    // [Route("/")]
+    //     public ActionResult Letter()
+    //     {
+    //       return View();
+    //     }
+    [Route("/journal")]
+    public ActionResult Journal()
+    {
+      return View();
+    }
+
+    [Route("/")]
+    public ActionResult Hello()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient("Lina");
+      myLetterVariable.SetSender("John");
+      return View(myLetterVariable);
+    }
+
+    [Route("/form")]
+    public ActionResult Form()
+    {
+      return View();
+    }
+
+    [Route("/greeting_card")]
+    public ActionResult GreetingCard()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient(Request.Query["recipient"]);
+      myLetterVariable.SetSender(Request.Query["sender"]);
+      myLetterVariable.SetVacationSpot(Request.Query["vacationspot"]);
+      myLetterVariable.SetSouvenir(Request.Query["souvenir"]);
+      return View("Hello", myLetterVariable);
+    }
+  }
+}
